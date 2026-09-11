8aed0b4ded41f0b7af0551101c5b6483f30f7f0c
amonozod <diyorilhomoff@gmail.com>
2026-09-11 14:41:07 +0500

    change

--- assets/auth.js
+++ assets/auth.js
@@ -10,9 +10,10 @@ async function getSession() {
 }
 
 async function signInWithEmail(email) {
+  const cleanUrl = window.location.origin + window.location.pathname;
   const { error } = await supabaseClient.auth.signInWithOtp({
     email,
-    options: { emailRedirectTo: window.location.href }
+    options: { emailRedirectTo: cleanUrl }
   });
   if (error) throw error;
 }
